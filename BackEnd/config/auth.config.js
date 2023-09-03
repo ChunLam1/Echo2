2b440a558e54ba02743ca3b63d4c1516852c97b6
Chun Lam <Chun01@hotmail.fr>
2023-09-03 22:35:25 +0200

    toto

--- BackEnd/config/auth.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secret: "yep-secret-key",
-  jwtExpiration: 3600000000, // 1 hour
-  jwtRefreshExpiration: 86400000000, // 24 hours
-};
--- BackEnd/routes/search.js
+++
@@ -1,8 +0,0 @@
-const searchController = require("../controllers/searchController.js");
-const express = require("express");
-
-const router = express.Router();
-
-router.route("/audio").get(searchController.searchByKeywords);
-
-module.exports = router;
